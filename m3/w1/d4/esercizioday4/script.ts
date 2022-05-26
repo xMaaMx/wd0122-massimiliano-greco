93355f998b83d521a62905afecb538ced2d64b7c
xMaaMx <massig87@live.it>
2022-05-26 17:58:06 +0200

    esercizioday4

--- m3/w1/d4/esercizioday4/script.ts
+++ m3/w1/d4/esercizioday4/script.ts
@@ -1,6 +1,8 @@
 const APPURL:string ='Abbigliamento.json'
 
-
+fetch("Abbigliamento.json")
+.then(res => res.json())
+.then(res => {
 
 class Vestiti{
     
@@ -17,6 +19,7 @@ class Vestiti{
     public saldo:number
     
     constructor(id:number,cod:number,coll:number,ca:string,mo:string,qu:number,col:string,pie:number,pii:number, di:string, sa:number){
+        
         fetch('./Abbigliamento.json')
         .then(data => data.json())
         .then(data => {
@@ -27,6 +30,7 @@ class Vestiti{
                 console.log(user);
                 
             }
+
         })
     }
 
@@ -34,87 +38,17 @@ class Vestiti{
         return this.prezzoivainclusa - this.prezzoivaesclusa
 
     }
+    getsaldocapo():number{
+                let sconto = ((this.prezzoivainclusa * this.saldo)/100)
+                return sconto
+            }
+            getacquistocapo(){
+                let caposcontato = this.prezzoivainclusa - ((this.prezzoivainclusa * this.saldo)/100)
+                return caposcontato
+            }
 
 }
 let spesa:any = new Vestiti(1,3,4,'maglia','polo',4,'red',10, 15, 'si', 50)
 console.log(spesa);
-
-
-/*fetch("Abbigliamento.json")
-.then(res => res.json())
-.then(res => {
-    
-console.log(res)
-for(let abbigliamento of res){
-class Abbigliamento{
-    public id:number ;
-    public codprod:number;
-    public collezione:string;
-    public capo:string;
-    public modello:number;
-    public quantita:number;
-    public colore:string;
-    public prezzoivaesclusa:number;
-    public prezzoivainclusa:number;
-    public disponibilità:string;
-    public saldo:number;
-    constructor(id:number,codprod:number,collezione:string,capo:string,modello:number,quantita:number,colore:string,prezzoivaesclusa:number,prezzoivainclusa:number,disponibilità:string,saldo:number){
-        this.id = id;
-        this.codprod = codprod;
-        this.collezione = collezione;
-        this.capo = capo;
-        this.modello = modello;
-        this.quantita = quantita;
-        this.colore = colore;
-        this.prezzoivaesclusa = prezzoivaesclusa;
-        this.prezzoivainclusa = prezzoivainclusa;
-        this.disponibilità = disponibilità;
-        this.saldo = saldo;
-    }
-    getsaldocapo():number{
-        let sconto = ((this.prezzoivainclusa * this.saldo)/100)
-        return sconto
-    }
-    getacquistocapo(){
-        let caposcontato = this.prezzoivainclusa - ((this.prezzoivainclusa * this.saldo)/100)
-        return caposcontato
-    }
 }
-let abbigliamento1:any = new Abbigliamento(abbigliamento.id,abbigliamento.codprod,abbigliamento.collezione,abbigliamento.capo,abbigliamento.modello,abbigliamento.quantita,abbigliamento.colore,abbigliamento.prezzoivaesclusa,abbigliamento.prezzoivainclusa,abbigliamento.disponibilità,abbigliamento.saldo)
-
-console.log(abbigliamento1)
-console.log('sconto: ' + abbigliamento1.getsaldocapo()+'€')
-console.log('vestito con sconto applicato: ' +'('+ abbigliamento1.prezzoivainclusa +'€'+ ' - ' + abbigliamento1.getsaldocapo() +'€'+') ' +'= '+ abbigliamento1.getacquistocapo()+'€')
-}
-
-})*/
-
-
-
-
-
-
-
-
-
-/*class Abbigliamento {
-    
-
-    constructor(id:number,cod:number,coll:number,ca:string,mo:string,qu:number,col:string,pie:number,pii:number, di:string, sa:number){
-
-        this.id = id
-        this.codprod = cod
-        this.collezione = coll
-        this.capo = ca
-        this.modello = mo
-        this.quantita = qu
-        this.colore = col
-        this.prezzoivaesclusa = pie
-        this.prezzoivainclusa = pii
-        this.disponibile = di
-        this.saldo = sa
-    }
-
-     
-}*/
-
+)
